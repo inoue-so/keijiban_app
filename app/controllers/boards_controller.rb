3cb23499f22c6c7c12d1dc0615cf96dfdb5ef863
inoue-so <knight0922basket@gmail.com>
2016-08-28 01:15:20 +0900

    remove boards/update.html.erb

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -14,12 +14,17 @@ class BoardsController < ApplicationController
   end
 
   def show
+ 　@board = Board.find(params[:id])
   end
 
   def edit
+    @board = Board.find(params[:id])
   end
 
   def update
+     @board = Board.find(params[:id])
+　   @board.update_attributes(params[:board])
+     redirect_to board_path(@board)
   end
 
   def destroy
